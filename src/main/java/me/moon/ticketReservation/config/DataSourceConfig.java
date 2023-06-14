25298c5c64970459a91ed48e0d53151112e1bafc
MoonHongMyeong <moonhongmyeong@gmail.com>
2023-06-14 16:58:47 +0900

    add : h2, mysql 종속성 추가 [#1]

---
+++ src/main/java/me/moon/ticketReservation/config/DataSourceConfig.java
@@ -0,0 +1,11 @@
+package me.moon.ticketReservation.config;
+
+import org.mybatis.spring.annotation.MapperScans;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//
+//@MapperScans()
+
+public class DataSourceConfig {
+}
