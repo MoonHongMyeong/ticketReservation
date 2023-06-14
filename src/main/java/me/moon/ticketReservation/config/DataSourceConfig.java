a96121259e128fc8ff01e4f1ccf0d8d0a17725a6
MoonHongMyeong <moonhongmyeong@gmail.com>
2023-06-14 19:57:43 +0900

    add : mybatis를 사용하기 위한 sqlSessionFactory, sqlSessionTemplate 정의 [#1]

--- src/main/java/me/moon/ticketReservation/config/DataSourceConfig.java
+++ src/main/java/me/moon/ticketReservation/config/DataSourceConfig.java
@@ -1,11 +1,34 @@
 package me.moon.ticketReservation.config;
 
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScans;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-//
-//@MapperScans()
+import javax.sql.DataSource;
+import java.io.IOException;
 
+@Configuration
+@RequiredArgsConstructor
 public class DataSourceConfig {
+    private final ApplicationContext applicationContext;
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setMapperLocations(
+                applicationContext.getResources("classpath:/mapper/*.xml")
+        );
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory){
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
 }
