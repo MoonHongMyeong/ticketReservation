1b344d6c794d1d96e4595740b1ad7cf0192bd1e3
MoonHongMyeong <moonhongmyeong@gmail.com>
2023-06-14 15:36:48 +0900

    init project

---
+++ src/main/java/me/moon/ticketRerservation/TicketRerservationApplication.java
@@ -0,0 +1,13 @@
+package me.moon.ticketRerservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketRerservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketRerservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/moon/ticketRerservation/TicketRerservationApplicationTests.java
@@ -0,0 +1,13 @@
+package me.moon.ticketRerservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketRerservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
