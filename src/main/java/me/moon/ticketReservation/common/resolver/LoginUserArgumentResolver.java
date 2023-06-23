2097ef2ed375fdb6349c7b3c273db4456f0a8b07
moonhongmyeong <moonhongmyeong@gmail.com>
2023-06-23 23:41:26 +0900

    add : SessionUser 정보를 가져오기 위한 @LoginUser 어노테이션을 정의하고 resolver에 추가 [#8]

---
+++ src/main/java/me/moon/ticketReservation/common/resolver/LoginUserArgumentResolver.java
@@ -0,0 +1,31 @@
+package me.moon.ticketReservation.common.resolver;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import me.moon.ticketReservation.login.annotation.LoginUser;
+import me.moon.ticketReservation.login.entity.SessionUser;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+    private final HttpSession session;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = parameter.getParameterType().equals(SessionUser.class);
+
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return session.getAttribute("USER");
+    }
+}
